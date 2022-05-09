c9b3578a07136f4f3e0c78796117baa28ca50ca8
im-nrm <aitorfernandezsm@gmail.com>
2022-05-09 20:07:43 +0200

    create Project

--- Project/src/app/app.component.ts
+++ Project/src/app/app.component.ts
@@ -2,8 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <!--HEADER-->
+    <app-header></app-header>
+    <!--ROUTE-->
+    <app-sidenav></app-sidenav>
+    <router-outlet></router-outlet>
+
+    <!--FOOTER-->
+    <app-footer></app-footer>
+  `,
+  styles: []
 })
 export class AppComponent {
   title = 'Project';
