5c2d7acd5507fbe47638eac8636304bb624c519c
DaniBokerone <danibokerone@gmail.com>
2022-06-06 21:05:32 +0200

    Creacion de la app para android

---
+++ Project/android/app/src/main/java/com/favi/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.favi.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ Project/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.favi.app',
+  appName: 'Favi',
+  webDir: 'dist/project',
+  bundledWebRuntime: false
+};
+
+export default config;
